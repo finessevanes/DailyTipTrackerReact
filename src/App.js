20e0e0b9a9ddd5296af0d07c1839d4adbd184d6f
Vanessa Mercado <vanessa.mercado24@gmail.com>
2019-10-08 22:21:01 -0700

    added links and new routing

--- src/App.js
+++ src/App.js
@@ -1,50 +1,64 @@
 import React from 'react';
 import './App.css';
 import DailyTipOutForm from './components/DailyTipOutForm'
-import Moment from 'react-moment';
-import 'moment-timezone';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import ShowDailyTipOuts from './components/ShowDailyTipOuts'
+import ShowDailyTipOut from './components/ShowDailyTipOut'
 
-class App extends React.Component{
-  constructor(){
-    super()
-    this.state = {
-      dailyTipOut:[]
-    }
+const About=()=> {
+  return <h2>About</h2>;
+}
+
+ class App extends React.Component{
+   constructor(){
+     super()
+     this.state= {
+       dailyTipOuts:[]
+     }
+   }
+  getDataFromAPI=()=>{
+    fetch("http://localhost:8080/dailytipouts")
+    .then((res) => res.json())
+    .then((response)=>{
+      this.setState({dailyTipOuts:response});
+    });
   }
-
-getDataFromAPI=()=>{
-  fetch("http://localhost:8080/dailytipouts")
-  .then((res) => res.json())
-  .then((response)=>{
-    this.setState({dailyTipOut:response});
-  });
-}
-tipHandleClick=(id)=>{
-  fetch("http://localhost:8080/dailytipout/" + id, {
-    method: 'delete',
-  }).then(()=>{
-    this.getDataFromAPI();
-  })
-}
   componentDidMount(){
     this.getDataFromAPI();
   }
   render(){
-    let tipOutsElementArr = this.state.dailyTipOut.map((tip)=>{
-      return <div
-      key={tip.id}>
-      Date: {tip.date}
-      Tip Amount: {tip.tipAmount}
-      Savings Rate: {tip.savingsRate*100}%
-      Amount Saved: {tip.amountSaved}
-      <button onClick={(event)=>this.tipHandleClick(tip.id)}>Delete Entry</button></div>
-    });
     return (
-      <div>
-        <div>{tipOutsElementArr}</div>
-        <DailyTipOutForm getDataFromAPI={this.getDataFromAPI}/>
-      </div>
-    );
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link id="show_daily_tip_outs" to="/">**Show All Tips**</Link>
+              </li>
+              <li>
+                <Link id="create_daily_tip_out" to="/create">__Add Tips__</Link>
+              </li>
+            </ul>
+          </nav>
+          <div id="content_body">
+          <Switch>
+          <Route path="dailytipout/:id" render={(props)=>(
+            <ShowDailyTipOut {...props} getDataFromAPI={this.getDataFromAPI}/>
+          )}/>
+          <Route path="/edit/dailytipout/:id" render={(props)=>(
+            <DailyTipOutForm {...props} getDataFromAPI={this.getDataFromAPI}/>
+          )}/>
+          <Route path="/create">
+            <DailyTipOutForm getDataFromAPI={this.getDataFromAPI} />
+            </Route>
+          <Route exact path="/">
+            <ShowDailyTipOuts getDataFromAPI={this.getDataFromAPI} dailyTipOuts={this.state.dailyTipOuts} />
+          </Route>
+          </Switch>
+          </div>
+        </div>
+      </Router>
+  );
   }
 }
 
